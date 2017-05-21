dee7481f8fa23b4749fde102a0ecdbf2986b6d64
Al Rainsbury <admin@alastairsmacbook.local>
2017-05-21 16:11:23 +0100

    2nd commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("lets push to GitHub")
+        print("lets push to GitHubbbb")
     }
 
     override func didReceiveMemoryWarning() {
